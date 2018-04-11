b9108a7b23a1d398525aa64a1fc60f5bd70ca55a
rasino <storm.45r@gmail.com>
2018-04-11 14:47:37 +0900

    Update cent6.9-setup.sh

--- cent6.9-setup.sh
+++ cent6.9-setup.sh
@@ -6,4 +6,5 @@ mkdir -p ~/src/data
 cd ~/src/data
 wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
+wget http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_6/shells:fish:release:2.repo
 yum -y install fish tmux
