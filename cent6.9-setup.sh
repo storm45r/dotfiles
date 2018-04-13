d47fd9632ac19ac98b2aad86d8115c05edd990d3
rasino <storm.45r@gmail.com>
2018-04-13 10:17:44 +0900

    Update cent6.9-setup.sh

--- cent6.9-setup.sh
+++ cent6.9-setup.sh
@@ -6,10 +6,13 @@ yum -y update
 #yum -y groupinstall "Desktop"
 #yum -y groupinstall "Japanese Support"
 #echo "export LANG=ja_JP.UTF-8
+
 mkdir -p ~/src/data
 cd ~/src/data
-#wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
-curl -O http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_6/shells:fish:release:2.repo
+wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
 rpm -ivh epel-release-6-8.noarch.rpm
+cd /etc/yum.repos.d/
+wget http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_6/shells:fish:release:2.repo
+cd
 yum -y install fish tmux
 
