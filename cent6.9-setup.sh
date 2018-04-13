be16b7af5d08a593607b3522c23a00f7b645e5bb
rasino <storm.45r@gmail.com>
2018-04-13 10:14:48 +0900

    Update cent6.9-setup.sh

--- cent6.9-setup.sh
+++ cent6.9-setup.sh
@@ -2,6 +2,10 @@
 yum -y update
 #yum -y groupinstall "Development tools"
 #yum -y groupinstall "Base"
+#yum -y groupinstall "X Window System"
+#yum -y groupinstall "Desktop"
+#yum -y groupinstall "Japanese Support"
+#echo "export LANG=ja_JP.UTF-8
 mkdir -p ~/src/data
 cd ~/src/data
 #wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
