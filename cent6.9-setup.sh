0e00a7b0b078687fb673e003f0844aafce3394b7
rasino <storm.45r@gmail.com>
2018-04-13 10:01:28 +0900

    Update cent6.9-setup.sh

--- cent6.9-setup.sh
+++ cent6.9-setup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 yum -y update
-yum -y groupinstall "Development tools"
-yum -y groupinstall "Base"
+#yum -y groupinstall "Development tools"
+#yum -y groupinstall "Base"
 mkdir -p ~/src/data
 cd ~/src/data
-wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+#wget http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm
+curl -O http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_6/shells:fish:release:2.repo
 rpm -ivh epel-release-6-8.noarch.rpm
-wget http://download.opensuse.org/repositories/shells:fish:release:2/CentOS_6/shells:fish:release:2.repo
 yum -y install fish tmux
+
